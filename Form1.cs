6df2a417bbb532976963639775fc6b6bf2a08804
nzue-wow <N.zuehlke.inf24@stud.bbbaden.ch>
2025-01-17 11:30:28 +0100

    Add files via upload

--- Form1.cs
+++ Form1.cs
@@ -8,17 +8,36 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace LP3
+namespace CalendarLP3.try2
 {
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
+            
         }
 
-        private void label1_Click(object sender, EventArgs e)
+        
+
+        private void Form1_Load(object sender, EventArgs e)
         {
+
+        }
+
+        private void displaDays()
+        {
+            DateTime now = DateTime.Now;
+
+            //First day of the month
+
+            DateTime startofthemonth = new DateTime(now.Year, now.Month,1);
+
+            //count of days of the month
+
+            int days = DateTime.DaysInMonth(now.Year, now.Month);
+            int dayoftheweek = ConvertToInt32(startofthemonth.DayOfWeek.ToString("d"));
+
 
         }
     }
---
+++ UserControlblank.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalendarLP3.try2
+{
+    public partial class UserControlblank : UserControl
+    {
+        public UserControlblank()
+        {
+            InitializeComponent();
+        }
+    }
+}
